464a284c59fbe0918e08efbce97f5b7c33f70662
Kathrin Höller <khoeller@terrestris.de>
2024-04-12 09:27:58 +0200

    einfache funktionsfähige Map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -299,20 +299,6 @@ export type MapCompProps = {
 
 export default function MapComp ({ map }: MapCompProps) {
 
-    // console.log(map)
-    // Beispiel: 
-    // console.log('Hallo aus der Map');
-    // const [myText, setMyText] = useState('Hallo Welt');
-    // useEffect(() => {
-    //     window.setTimeout(() => {
-    //         setMyText('Gesundheit!');
-    //     }, 4000);    
-    // }, []);
-    // const myFunc = () => {
-    //     console.log('dwadd')
-    // };
-    // myFunc()                                              
-
     useEffect(() => {       // function wird nur einmal initial aufgerufen; in den eckigen Klammern kann man eine Bedingung eingeben, bei der es erneut aufgerufen wird z.B. map, wenn sich die map ändert
         map.setTarget('map');
     }, [map]); 
@@ -329,37 +315,5 @@ export default function MapComp ({ map }: MapCompProps) {
     );
 }
 
-// MapContainer mit einer Überschrift (Heading 1) und einer Karte 
-
-
-// CSS
-    // {
-    //     position: fixed;
-    //     top: 0; 
-    //     left: 0;
-    //     width: 100%;
-    //     height: 100%;
-    //     font-family: sans-serif;
-    // }
-
-// Alternative zu extra CSS-Datei:
-    // export default function Map () {
-    //     return (
-    //         <div 
-    //           id='map' 
-    //           className ='map'
-    //           style={{
-    //             position: 'fixed',
-    //             top: 0, 
-    //             left: 0,
-    //             width: '100%',
-    //             height: '100%',
-    //             fontFamily: 'sans-serif' 
-    //           }}
-    //         >
-    
-    //         </div>
-    //     );
-    // }
 
     
